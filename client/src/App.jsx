06f68eb261c29ad31578a3867e5c41f64c6d8333
Atharva Navgire <atharvanavgire10@gmail.com>
2026-07-21 19:30:31 +0530

    Feature: Pharmacy Dashboard UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,4 @@
 // import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './layouts/LandingLayout';
 import "./styles/landing.css";
 import AppRoutes from './routes/AppRoutes';
 function App() {
