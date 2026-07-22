3b1fbe3a54844c0e500005b0010064370556b35b
Darshan <darshangaikwad6712@gmail.com>
2026-07-22 15:47:07 +0530

    Feature: Implemented Users Authentication

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -24,6 +25,8 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      minlength: 6,
+      select: false,
     },
 
     role: {
@@ -48,5 +51,26 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-export default mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre("save", async function () {
 
+  if (!this.isModified("password")) return;
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+
+
+});
+
+// Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+
+  return await bcrypt.compare(
+    enteredPassword,
+    this.password
+  );
+
+};
+
+export default mongoose.model("User", userSchema);
