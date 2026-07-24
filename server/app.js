bfffdf2c57710c3d14e20692352d443cd4e2c5ea
Atharva Navgire <atharvanavgire10@gmail.com>
2026-07-24 18:08:50 +0530

    Feature: Implemented Dashboard API's Model

--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ import medicalReportRoutes from "./src/routes/medicalReport.routes.js";
 import path from "path";
 import receptionistRoutes from "./src/routes/receptionist.routes.js";
 import pharmacyRoutes from "./src/routes/pharmacy.routes.js";
+import dashboardRoutes from "./src/routes/dashboard.routes.js";
 
 
 
@@ -52,6 +53,7 @@ app.use("/api/medical-reports",medicalReportRoutes);
 app.use("/uploads",express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/receptionist", receptionistRoutes);
 app.use("/api/pharmacy", pharmacyRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 
 export default app;
